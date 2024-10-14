9ddd7c04184eb765c9fbff85c1bc673284a05274
luanuink <luan.live2011@gmail.com>
2024-10-14 18:57:31 -0300

    atualizando Workflows

--- src/test/java/br/com/fiap/trafego/AcidenteApplicationTests.java
+++ src/test/java/br/com/fiap/trafego/AcidenteApplicationTests.java
@@ -2,12 +2,16 @@ package br.com.fiap.trafego;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("dev") // Adicione esta linha para ativar o perfil 'dev'
 @SpringBootTest
 class AcidenteApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+   AcidenteApplicationTests() {
+   }
 
+   @Test
+   void contextLoads() {
+   }
 }
